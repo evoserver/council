d2c94a4c59cf084bddc18f84bbc8d6e7ff5a48ad
evoserver <mixology@tiscali.it>
2023-02-16 15:39:35 +0100

    Highlighting PR

--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -1,101 +1,106 @@
 <template>
-    <div :id="'reply-'+id" class="panel" :class="isBest ? 'panel-success': 'panel-default'">
-        <div class="panel-heading">
-            <div class="level">
-                <h5 class="flex">
-                    <a :href="'/profiles/' + reply.owner.name"
-                        v-text="reply.owner.name">
-                    </a> said <span v-text="ago"></span>
-                </h5>
+  <div :id="'reply-'+id" class="panel" :class="isBest ? 'panel-success': 'panel-default'">
+    <div class="panel-heading">
+      <div class="level">
+        <h5 class="flex">
+          <a :href="'/profiles/' + reply.owner.name"
+             v-text="reply.owner.name">
+          </a> said <span v-text="ago"></span>
+        </h5>
 
-                <div v-if="signedIn">
-                    <favorite :reply="reply"></favorite>
-                </div>
-            </div>
+        <div v-if="signedIn">
+          <favorite :reply="reply"></favorite>
         </div>
-
-        <div class="panel-body">
-            <div v-if="editing">
-                <form @submit="update">
-                    <div class="form-group">
-                        <wysiwyg v-model="body"></wysiwyg>
-                    </div>
-
-                    <button class="btn btn-xs btn-primary">Update</button>
-                    <button class="btn btn-xs btn-link" @click="editing = false" type="button">Cancel</button>
-                </form>
-            </div>
-
-            <div v-else v-html="body"></div>
-        </div>
-
-        <div class="panel-footer level" v-if="authorize('owns', reply) || authorize('owns', reply.thread)">
-            <div v-if="authorize('owns', reply)">
-                <button class="btn btn-xs mr-1" @click="editing = true" v-if="! editing">Edit</button>
-                <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
-            </div>
-
-            <button class="btn btn-xs btn-default ml-a" @click="markBestReply" v-if="authorize('owns', reply.thread)">Best Reply?</button>
-        </div>
+      </div>
     </div>
+
+    <div class="panel-body">
+      <div v-if="editing">
+        <form @submit="update">
+          <div class="form-group">
+            <wysiwyg v-model="body"></wysiwyg>
+          </div>
+
+          <button class="btn btn-xs btn-primary">Update</button>
+          <button class="btn btn-xs btn-link" @click="editing = false" type="button">Cancel</button>
+        </form>
+      </div>
+
+      <div ref="body" v-else>
+        <highlight :content="body"></highlight>
+      </div>
+    </div>
+
+    <div class="panel-footer level" v-if="authorize('owns', reply) || authorize('owns', reply.thread)">
+      <div v-if="authorize('owns', reply)">
+        <button class="btn btn-xs mr-1" @click="editing = true" v-if="! editing">Edit</button>
+        <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
+      </div>
+
+      <button class="btn btn-xs btn-default ml-a" @click="markBestReply" v-if="authorize('owns', reply.thread)">Best
+        Reply?
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Favorite from './Favorite.vue';
-    import moment from 'moment';
+  import Favorite from './Favorite.vue';
+  import Highlight from './Highlight.vue';
+  import moment from 'moment';
 
-    export default {
-        props: ['reply'],
+  export default {
+    props: ['reply'],
 
-        components: { Favorite },
+    components: {Favorite, Highlight},
 
-        data() {
-            return {
-                editing: false,
-                id: this.reply.id,
-                body: this.reply.body,
-                isBest: this.reply.isBest,
-            };
-        },
+    data() {
+      return {
+        editing: false,
+        id: this.reply.id,
+        body: this.reply.body,
+        isBest: this.reply.isBest,
+      };
+    },
 
-        computed: {
-            ago() {
-                return moment(this.reply.created_at).fromNow() + '...';
-            }
-        },
+    computed: {
+      ago() {
+        return moment(this.reply.created_at).fromNow() + '...';
+      }
+    },
 
-        created () {
-            window.events.$on('best-reply-selected', id => {
-                this.isBest = (id === this.id);
+    created() {
+      window.events.$on('best-reply-selected', id => {
+        this.isBest = (id === this.id);
+      });
+    },
+
+    methods: {
+      update() {
+        axios.patch(
+            '/replies/' + this.id, {
+              body: this.body
+            })
+            .catch(error => {
+              flash(error.response.data, 'danger');
             });
-        },
 
-        methods: {
-            update() {
-                axios.patch(
-                    '/replies/' + this.id, {
-                        body: this.body
-                    })
-                    .catch(error => {
-                        flash(error.response.data, 'danger');
-                    });
+        this.editing = false;
 
-                this.editing = false;
+        flash('Updated!');
+      },
 
-                flash('Updated!');
-            },
+      destroy() {
+        axios.delete('/replies/' + this.id);
 
-            destroy() {
-                axios.delete('/replies/' + this.id);
+        this.$emit('deleted', this.id);
+      },
 
-                this.$emit('deleted', this.id);
-            },
+      markBestReply() {
+        axios.post('/replies/' + this.id + '/best');
 
-            markBestReply() {
-                axios.post('/replies/' + this.id + '/best');
-
-                window.events.$emit('best-reply-selected', this.id);
-            }
-        }
+        window.events.$emit('best-reply-selected', this.id);
+      }
     }
+  }
 </script>
--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -1,56 +1,57 @@
 <script>
-    import Replies from '../components/Replies.vue';
-    import SubscribeButton from '../components/SubscribeButton.vue';
+import Replies from '../components/Replies.vue';
+import SubscribeButton from '../components/SubscribeButton.vue';
+import Highlight from '../components/Highlight.vue';
 
-    export default {
-        props: ['thread'],
+export default {
+  props: ['thread'],
 
-        components: {Replies, SubscribeButton},
+  components: {Replies, SubscribeButton, Highlight},
 
-        data () {
-            return {
-                repliesCount: this.thread.replies_count,
-                locked: this.thread.locked,
-                title: this.thread.title,
-                body: this.thread.body,
-                form: {},
-                editing: false
-            };
-        },
+  data() {
+    return {
+      repliesCount: this.thread.replies_count,
+      locked: this.thread.locked,
+      title: this.thread.title,
+      body: this.thread.body,
+      form: {},
+      editing: false
+    };
+  },
 
-        created () {
-            this.resetForm();
-        },
+  created() {
+    this.resetForm();
+  },
 
-        methods: {
-            toggleLock () {
-                let uri = `/locked-threads/${this.thread.slug}`;
+  methods: {
+    toggleLock() {
+      let uri = `/locked-threads/${this.thread.slug}`;
 
-                axios[this.locked ? 'delete' : 'post'](uri);
+      axios[this.locked ? 'delete' : 'post'](uri);
 
-                this.locked = ! this.locked;
-            },
+      this.locked = !this.locked;
+    },
 
-            update () {
-                let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
+    update() {
+      let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
 
-                axios.patch(uri, this.form).then(() => {
-                    this.editing = false;
-                    this.title = this.form.title;
-                    this.body = this.form.body;
+      axios.patch(uri, this.form).then(() => {
+        this.editing = false;
+        this.title = this.form.title;
+        this.body = this.form.body;
 
-                    flash('Your thread has been updated.');
-                })
-            },
+        flash('Your thread has been updated.');
+      })
+    },
 
-            resetForm () {
-                this.form = {
-                    title: this.thread.title,
-                    body: this.thread.body
-                };
+    resetForm() {
+      this.form = {
+        title: this.thread.title,
+        body: this.thread.body
+      };
 
-                this.editing = false;
-            }
-        }
+      this.editing = false;
     }
+  }
+}
 </script>
